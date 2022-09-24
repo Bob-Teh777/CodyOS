64e3438207eb8a75f54b06b0596935522f38ddec
Bob-Teh777 <114314463+Bob-Teh777@users.noreply.github.com>
2022-09-25 10:32:48 +1300

    Update index.js

--- Boot/index.js
+++ Boot/index.js
@@ -1,5 +1,4 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
+const { ipcMain } = require('electron')
 
 const createWindow = () => {
   const win = new BrowserWindow({
@@ -8,23 +7,7 @@ const createWindow = () => {
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
     },
-  });
-
-  win.loadFile('index.html');
-};
-
-app.whenReady().then(() => {
-  createWindow();
-
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow();
-    }
-  });
-});
-
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
+  })
+  ipcMain.handle('ping', () => 'pong')
+  win.loadFile('index.html')
+}
